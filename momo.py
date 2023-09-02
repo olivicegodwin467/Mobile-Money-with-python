95fe0eadf57fa54071377c2874fa18f5e2539ec8
olivicegodwin467 <128424071+olivicegodwin467@users.noreply.github.com>
2023-09-02 09:10:54 -0700

    Update my code with new class and while loop

--- momo.py
+++ momo.py
@@ -1,45 +1,48 @@
-
+class Momo():
+    def __init__(self,user,phone_number):
+        pass
 ussd = input('>> ')
-if ussd == '*182#':
+while True:
+    if ussd == '*182#':
 
-    print('Notification')
-    print(' ')
-    print('''
-    1. Kohereza Amafranga
-    2. Kugura
-    3. Kwishyura
-    4. Serivisi za Bank
-    5. Inguzanyo no kuzigama
-    6. Konti Yanjye ya MoMo
-    7. Ibyemezo bitaranozwa
-    n Next
-    ''')
-
-    next = input('>> ')
-    if next == 'n':
-        
+        print('Notification')
+        print(' ')
         print('''
-        8. MoMoPay
-        9. Sekirite Yanjye ya MoMo
-        10. Ubwishingizi
-        11. Amategeko namabyiriza bya MoMo
-        12. Macye Macye
-        13. Babyl Health
-        14. Bariza Hano
-        15. Sohokamo
+        1. Kohereza Amafranga
+        2. Kugura
+        3. Kwishyura
+        4. Serivisi za Bank
+        5. Inguzanyo no kuzigama
+        6. Konti Yanjye ya MoMo
+        7. Ibyemezo bitaranozwa
+        n Next
         ''')
 
-choice = input('>> ')
-if choice == '6':
-    print('''
-    
-    1. Kureba Asigaye
-    2. Raporo ngufi
-    3. Kubona Imibare ya Cash power iherutse
-    4. Preapproved Transactions
-    5. Guhindura ururimi
-    6. My Offers
-    ''')
-else:
-    print('Exaternal application down')
-print('')
+        next = input('>> ')
+        if next == 'n':
+            
+            print('''
+            8. MoMoPay
+            9. Sekirite Yanjye ya MoMo
+            10. Ubwishingizi
+            11. Amategeko namabyiriza bya MoMo
+            12. Macye Macye
+            13. Babyl Health
+            14. Bariza Hano
+            15. Sohokamo
+            ''')
+
+    choice = input('>> ')
+    if choice == '6':
+        print('''
+        
+        1. Kureba Asigaye
+        2. Raporo ngufi
+        3. Kubona Imibare ya Cash power iherutse
+        4. Preapproved Transactions
+        5. Guhindura ururimi
+        6. My Offers
+        ''')
+    else:
+        print('Exaternal application down')
+    print('')
