11476e4bcce276c79913dc45afbf0fccf96ea0dd
olivicegodwin467 <128424071+olivicegodwin467@users.noreply.github.com>
2023-09-02 10:00:07 -0700

    Update momo.py

--- momo.py
+++ momo.py
@@ -1,48 +1,67 @@
+name = 'Twizerimana olivier'
+phone = 785273109
+pin = 12345
+
 class Momo():
-    def __init__(self,user,phone_number):
-        pass
-ussd = input('>> ')
+    def __init__(self,user,phone_number, pin):
+        self.user = name
+        self.phone_number = phone
+        self.pin = pin
+
+    def displayWelcome(self):
+        print('Welcome dear')
+        return self.user
+mm = Momo(name, phone, pin)
+print(mm.displayWelcome())
+ussd = input('USSD: ')
+if ussd == '*182#':
+
+    print('Notification')
+    print(' ')
+    print('''
+    1. Send Money
+    2. Buy
+    3. Pay Bill
+    4. Bank Services
+    5. Loans and Saving
+    6. My MoMo Account
+    7. Pending Approvals
+    8. MoMoPay
+    9. My MoMo Security
+    10. Insurance
+    11. MoMo Terms and Conditions
+    12. Macye Macye
+    13. Babyl Health
+    14. Help
+    15. Exit
+    ''')
+else:
+    print('External Application Down')
+    
 while True:
-    if ussd == '*182#':
-
-        print('Notification')
-        print(' ')
-        print('''
-        1. Kohereza Amafranga
-        2. Kugura
-        3. Kwishyura
-        4. Serivisi za Bank
-        5. Inguzanyo no kuzigama
-        6. Konti Yanjye ya MoMo
-        7. Ibyemezo bitaranozwa
-        n Next
-        ''')
-
-        next = input('>> ')
-        if next == 'n':
-            
-            print('''
-            8. MoMoPay
-            9. Sekirite Yanjye ya MoMo
-            10. Ubwishingizi
-            11. Amategeko namabyiriza bya MoMo
-            12. Macye Macye
-            13. Babyl Health
-            14. Bariza Hano
-            15. Sohokamo
-            ''')
-
     choice = input('>> ')
     if choice == '6':
         print('''
         
-        1. Kureba Asigaye
-        2. Raporo ngufi
-        3. Kubona Imibare ya Cash power iherutse
-        4. Preapproved Transactions
-        5. Guhindura ururimi
+        1. Check Balance
+        2. Mini Statement
+        3. Get Latest Electricity Token
+        4. Preapprove Treansactions
+        5. Change Language
         6. My Offers
+        7. Exit
+        0. Back
         ''')
+        choose = int(input('> '))
+        if choose == 1:
+            confirm_pin = int(input('To confim to get Balance, Enter MM PIN: '))
+            if confirm_pin == pin:
+                print('''
+
+                      Your balance is $3,000,450 and Welcome
+                      account balance is-. Thank you for using mobile money
+''')
+
     else:
         print('Exaternal application down')
-    print('')
+        break
